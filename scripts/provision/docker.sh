b6827890fc0eed511927f56d41f95d1acdf7aefc
mrporcles <mrporcles@gmail.com>
2018-03-22 11:44:02 +0000

    Updated to use dameon.json configuration file
    Updated to use dameon.json configuration file for making remote API
    available and defining insecure Harbor registry

--- scripts/provision/docker.sh
+++ scripts/provision/docker.sh
@@ -3,9 +3,15 @@
 set -e
 
 # Create defaults docker file
-echo "Adding DOCKER_OPTS (/etc/default/docker)"
-sudo cat <<EOF | sudo dd of=/etc/default/docker status=none
-DOCKER_OPTS="--insecure-registry harbor.local:80 -H unix:///var/run/docker.sock -H tcp://0.0.0.0:2375"
+echo "Adding DOCKER_OPTS (/etc/docker/daemon.json)"
+
+mkdir -p /etc/docker
+touch /etc/docker/daemon.json
+sudo cat <<EOF | sudo dd of=/etc/docker/daemon.json status=none
+{
+	"hosts": ["unix://var/run/docker.sock","tcp://0.0.0.0:2375"],
+	"insecure-registries": ["harbor.local:80"]
+}
 EOF
 
 # Enable docker service
