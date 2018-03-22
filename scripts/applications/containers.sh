250e16da759334d8f2c2f190c4083e4c5c574a4d
mrporcles <mrporcles@gmail.com>
2018-03-22 11:49:32 +0000

    Updated to deploy latest Admiral/Harbor versions
    Updated to deploy latest GA versions of Harbor and Admiral as packaged
    with VIC. Added Clair to Harbor deployment. Added admiral-cli to
    Admiral node. Updated version of docker-compose. Configure Admiral to
    allow adding insecure Harbor registry. This is now disabled by default.
    Install jq package for JSON manipulation. Add API requests to add
    Docker hosts and Harbor registry to Admiral

--- scripts/applications/containers.sh
+++ scripts/applications/containers.sh
@@ -3,8 +3,9 @@
 set -e
 
 # User defined variables
-harbor_version="0.3.5"
-docker_compose_version="1.8.0"
+admiral_version="v1.3.0"
+harbor_version="1.3.0"
+docker_compose_version="1.19.0"
 
 # Check for first argument (must be node name to map containers)
 function check_first_arg {
@@ -19,12 +20,50 @@ function check_first_arg {
 function install_admiral {
 	if [ $1 == "admiral" ]
 	then
+		echo "We need jq, installing..."
+		sudo tdnf install jq -y
+		if [ $? -ne 0 ]; then
+			echo "Could not install jq (tdnf), exiting."
+			exit 1
+		fi
+		echo "We need admiral cli, fetching"
+		sudo curl -sL https://github.com/mrporcles/admiral/raw/master/bin/admiral -o /usr/local/bin/admiral && sudo chmod +x /usr/local/bin/admiral
+		if [ $? -ne 0 ]; then
+			echo "Could not install admiral cli, exiting."
+			exit 1
+		fi
 		echo "Fetching VMware Admiral Container Image and starting container (ports 8282:8282)"
-		sudo docker pull vmware/admiral
+		sudo docker pull vmware/admiral:${admiral_version}
 		# TODO Fix static IP
-		sudo docker run -d -p 8282:8282 --add-host="harbor.local:192.168.33.102" --name admiral vmware/admiral
+		sudo docker run -d -p 8282:8282 --add-host="harbor.local:192.168.33.102" --name admiral -u root vmware/admiral:${admiral_version}
 		if [ $? -ne 0 ]; then
 			echo "Something went wrong starting VMware Admiral, exiting"
+			exit 1
+		fi
+    echo "Starting VMware Admiral Configuration"
+		sudo docker exec admiral sh -c "echo allow.registry.plain.http.connection=true >> /admiral/config/dist_configuration.properties"
+		if [ $? -ne 0 ]; then
+			echo "Something went wrong configuring VMware Admiral, exiting"
+			exit 1
+		fi
+		echo "Restarting Admiral to refresh configuration"
+		sudo docker stop admiral && sudo docker start admiral
+		if [ $? -ne 0 ]; then
+			echo "Something went wrong restarting VMware Admiral, exiting"
+			exit 1
+		fi
+		echo "Waiting 20s for Admiral to restart"
+		sleep 20s
+		echo "Adding Docker Hosts to VMware Admiral"
+		sudo curl -s -X POST http://192.168.33.101:8282/resources/clusters -H "x-project: /projects/default-project" -H "Content-Type: application/json" -d '{"hostState":{"address":"http://192.168.33.103:2375","customProperties":{"__containerHostType":"DOCKER","__adapterDockerType":"API","__clusterName":"docker-cluster"}},"acceptCertificate":true}' && cluster=$(sudo curl -s -X GET http://192.168.33.101:8282/resources/clusters |jq -r '.documentLinks | .[]') && sudo curl -s -X POST http://192.168.33.101:8282$cluster/hosts -H "x-project: /projects/default-project" -H "Content-Type: application/json" -d '{"hostState":{"address":"http://192.168.33.104:2375","customProperties":{"__containerHostType":"DOCKER","__adapterDockerType":"API"}},"acceptCertificate":false}'
+		if [ $? -ne 0 ]; then
+			echo "Something went wrong adding Docker Hosts to VMware Admiral, exiting"
+			exit 1
+		fi
+		echo "Adding Harbor Registry to VMware Admiral"
+		sudo curl -s -X POST http://192.168.33.101:8282/core/auth/credentials -H "x-project: /projects/default-project" -H "Content-Type: application/json" -d '{"type":"Password","userEmail":"admin","privateKey":"Harbor12345","customProperties":{"__authCredentialsName":"harbor-admin"}}' && credential=$(sudo curl -s -X GET http://192.168.33.101:8282/core/auth/credentials |jq '.documentLinks | .[]' |grep -v cert) && payload="{\"hostState\":{\"address\":\"http://harbor.local:80\",\"name\":\"Harbor\",\"endpointType\":\"container.docker.registry\",\"authCredentialsLink\":$credential}}" && sudo curl -s -X PUT http://192.168.33.101:8282/config/registry-spec -H "x-project: /projects/default-project" -H "Content-Type: application/json" -d $payload
+		if [ $? -ne 0 ]; then
+			echo "Something went wrong adding Harbor Registry to VMware Admiral, exiting"
 			exit 1
 		fi
 	fi
@@ -35,10 +74,17 @@ function install_harbor {
 	if [ $1 == "harbor" ]
 	then
 		echo "Cleaning up and fetching VMware Harbor Release (version ${harbor_version})"
-		sudo rm -rf /tmp/harbor*
-		sudo curl -o /tmp/harbor.tar.gz -Ls https://github.com/vmware/harbor/releases/download/${harbor_version}/harbor-installer.tgz
+		sudo rm -rf /opt/harbor*
+		sudo curl -o /opt/harbor.tar.gz -Ls https://storage.googleapis.com/harbor-releases/harbor-online-installer-v${harbor_version}.tgz
 		if [ $? -ne 0 ]; then
 			echo "Error retrieving file from the web"
+			exit 1
+		fi
+
+		echo "We need python2, installing..."
+		sudo tdnf install python2 -y
+		if [ $? -ne 0 ]; then
+			echo "Could not install python2 (tdnf), exiting."
 			exit 1
 		fi
 
@@ -59,12 +105,12 @@ function install_harbor {
 		sudo chmod +x /usr/local/bin/docker-compose
 
 		echo "Unpacking and configuring VMware Harbor"
-		sudo mkdir /tmp/harbor && sudo tar -xzf /tmp/harbor.tar.gz -C /tmp/harbor --strip-components=2
-                sudo sed -i "s/reg\.mydomain\.com/${1}.local/" /tmp/harbor/harbor.cfg
-                cd /tmp/harbor; ./prepare;
+		sudo mkdir /opt/harbor && sudo tar -xzf /opt/harbor.tar.gz -C /opt
+                sudo sed -i "s/reg\.mydomain\.com/${1}.local/" /opt/harbor/harbor.cfg
+                cd /opt/harbor; ./install.sh --with-clair;
 
-		echo "Calling docker-compose up"
-                sudo /usr/local/bin/docker-compose -f /tmp/harbor/docker-compose.yml up -d
+		#echo "Calling docker-compose up"
+    #            sudo /usr/local/bin/docker-compose -f /tmp/harbor/docker-compose.yml up -d
 
 		if [ $? -eq 0 ]; then
                         echo "Successfully started VMware Harbor..."
